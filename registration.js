f713d49baacd7fe8106e29c281ce1443a8dc8cfa
= <=>
2019-10-31 10:25:35 +0200

    begining

---
+++ registration.js
@@ -0,0 +1,13 @@
+module.exports = {
+    register,
+    login
+};
+
+function register(req, res) {
+    res.send();
+}
+
+function login(req, res) {
+    res.send();
+
+}
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const registration = require("./registration.js");
+
+const port = process.env.PORT || 80;
+const app = express();
+
+app.use(cookieParser());
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile('./public/pages/project.html', {
+    root: __dirname
+}));
+app.get('/registration', (req, res) => res.sendFile('./pages/Registration.html', {
+    root: __dirname
+}));
+app.get('/login', (req, res) => res.sendFile('./pages/login.html', {
+    root: __dirname
+}));
+app.get('/about', (req, res) => res.sendFile('./pages/about.html', {
+    root: __dirname
+}));
+
+app.post('/registration/register', (req, res) => {
+    return registration.register(req, res);
+});
+app.post('/registration/login', (req, res) => {
+    return registration.login(req, res);
+});
+
+app.listen(port, () => console.log('Example app listening on port ' + port));
