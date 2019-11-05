3691517cd87bc662571f235f168dc932d9976f4c
= <=>
2019-11-05 14:43:19 +0200

    last

--- sql.js
+++ sql.js
@@ -4,19 +4,19 @@ let db;
 
 mysql.createPool({
     connectionLimit: 100,
-    host: "localhost",
-    user: "root",
-    password: "beitar",
-    database: "my_school"
+    host: "eu-cdbr-west-02.cleardb.net",
+    user: "bee1beacd8e7b2",
+    password: "aff30418",
+    database: "heroku_8e032dbd0b28bea"
 }).then((c) => {
     db = c;
-}).then((my_school) => {
-    console.log(my_school)
+}).then((heroku_8e032dbd0b28bea) => {
+    console.log(heroku_8e032dbd0b28bea)
 }).catch((e) => {
     console.error(e);
 });
 
 module.exports = async function (req, res) {
-    let data = await db.query("SELECT * FROM my_school.`mehina-girls`")
+    let data = await db.query("SELECT * FROM heroku_8e032dbd0b28bea.`mehina-girls`");
     res.send(data);
 }
