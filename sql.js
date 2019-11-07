847225531f0e2f9c63b0f118108256a8b6efaffa
=keren <=katzen.keren@gmail.com>
2019-11-07 21:46:19 +0200

    heroku

--- sql.js
+++ sql.js
@@ -5,17 +5,17 @@ let db;
 mysql.createPool({
     connectionLimit: 100,
     host: "eu-cdbr-west-02.cleardb.net",
-    user: "bee1beacd8e7b2",
-    password: "aff30418",
-    database: "heroku_8e032dbd0b28bea"
+    user: "bb1b513d62e056",
+    password: "f8236513",
+    database: "hheroku_21ff2f1f46864dc"
 }).then((c) => {
     db = c;
-}).then((heroku_8e032dbd0b28bea) => {}).catch((e) => {
+}).then((heroku_21ff2f1f46864dc) => {}).catch((e) => {
     console.error(e);
 });
 
 module.exports = async function (req, res) {
-    let data = await db.query("SELECT * FROM heroku_8e032dbd0b28bea.`mehina-girls`");
+    let data = await db.query("SELECT * FROM heroku_21ff2f1f46864dc");
     console.log(data);
     res.send(data);
 }
