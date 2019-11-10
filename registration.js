bdd1c153d3a46035dab48a1eee055c3cb89f82a6
=keren <=katzen.keren@gmail.com>
2019-11-10 17:16:08 +0200

    newsqltable++++

--- registration.js
+++ registration.js
@@ -1,3 +1,20 @@
+const mysql = require('promise-mysql');
+let db;
+
+mysql.createPool({
+        connectionLimit: 100,
+        host: process.env.MYSQL_URL,
+        user: process.env.MYSQL_USER,
+        password: process.env.mysql_native_password,
+        database: process.env.MYSQL_DB
+    }).then((c) => {
+        db = c;
+
+    })
+    .catch((e) => {
+        console.error(e);
+    });
+
 async function register(req, res) {
     let users = await db.query("select * from users");
     for (let user of users) {
@@ -25,22 +42,6 @@ async function login(req, res) {
     res.status(500).send("Incorrect login credentials i.e. userName/email or password!")
 }
 
-const mysql = require('promise-mysql');
-let db;
-
-mysql.createPool({
-        connectionLimit: 100,
-        host: process.env.MYSQL_URL,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DB
-    }).then((c) => {
-        db = c;
-
-    })
-    .catch((e) => {
-        console.error(e);
-    });
 
 module.exports = {
     register,
--- sql.js
+++ sql.js
@@ -4,18 +4,19 @@ let db;
 
 mysql.createPool({
     connectionLimit: 100,
-    host: "eu-cdbr-west-02.cleardb.net",
-    user: "bb1b513d62e056",
-    password: "f8236513",
-    database: "hheroku_21ff2f1f46864dc"
+    host: process.env.MYSQL_URL,
+    user: process.env.MYSQL_USER,
+    password: process.env.mysql_native_password,
+    database: process.env.MYSQL_DB
 }).then((c) => {
     db = c;
-}).then((heroku_21ff2f1f46864dc) => {}).catch((e) => {
+
+}).catch((e) => {
     console.error(e);
 });
 
 module.exports = async function (req, res) {
-    let data = await db.query("SELECT * FROM heroku_21ff2f1f46864dc.`mehina_girls`");
+    let data = await db.query("SELECT * FROM `mehina_girls`");
     console.log(data);
     res.send(data);
 }
